283fbcb180faa2a9bd437470e35526e3d3ee1b58
caijianhaoa <1784581164@qq.com>
2023-01-04 00:16:15 +0800

    长度最小的子数组-滑动窗口

---
+++ 数组/长度最小的子数组.go
@@ -0,0 +1,26 @@
+package main
+
+func minSubArrayLen(target int, nums []int) int {
+	i := 0
+	length := len(nums)
+	res := length + 1
+	sum := 0
+
+	for j := 0; j < length; j++ {
+		sum += nums[j]
+		for sum >= target {
+			subLen := j - i + 1
+			if res > subLen {
+				res = subLen
+			}
+			sum -= nums[i]
+			i++
+		}
+	}
+
+	if res >= length+1 {
+		return 0
+	} else {
+		return res
+	}
+}
