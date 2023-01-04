56e7a19ae7693025d48a876fc3e9d3825ca62933
caijianhao <caijianhao@baidu.com>
2023-01-04 14:07:05 +0800

    螺旋矩阵II

---
+++ 数组/螺旋矩阵II.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func generateMatrix(n int) [][]int {
+	left, right := 0, n-1
+	top, bottom := 0, n-1
+	num := 1
+	tar := n * n
+	matrix := make([][]int, n)
+	for i := 0; i < n; i++ {
+		matrix[i] = make([]int, n)
+	}
+	for num <= tar {
+		for i := left; i <= right; i++ {
+			matrix[top][i] = num
+			num++
+		}
+		top++
+		for i := top; i <= bottom; i++ {
+			matrix[i][right] = num
+			num++
+		}
+		right--
+		for i := right; i >= left; i-- {
+			matrix[bottom][i] = num
+			num++
+		}
+		bottom--
+		for i := bottom; i >= top; i-- {
+			matrix[i][left] = num
+			num++
+		}
+		left++
+	}
+	return matrix
+}
+
+func main() {
+	fmt.Printf("generateMatrix(3): %v\n", generateMatrix(3))
+}
