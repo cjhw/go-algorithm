7a26c54842fa9802dbd64cc500f4a93659c220f0
cjhw <1784581164@qq.com>
2023-01-09 10:20:16 +0800

    删除字符串中的所有相邻重复项

---
+++ Stack/删除字符串中的所有相邻重复项.go
@@ -0,0 +1,16 @@
+package Stack
+
+func removeDuplicates(s string) string {
+
+	stack := make([]byte, 0)
+
+	for i := 0; i < len(s); i++ {
+		if len(stack) > 0 && s[i] == stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, s[i])
+		}
+	}
+
+	return string(stack)
+}
