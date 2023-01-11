e7ff8a74143fd7de46bba1de7f79b267df750658
cjhw <1784581164@qq.com>
2023-01-11 14:35:28 +0800

    二叉树的所有路径迭代法

--- Tree/二叉树的所有路径.go
+++ Tree/二叉树的所有路径.go
@@ -2,29 +2,64 @@ package Tree
 
 import "strconv"
 
+// func binaryTreePaths(root *TreeNode) []string {
+// 	res := make([]string, 0)
+
+// 	var traverse func(root *TreeNode, s string)
+
+// 	traverse = func(root *TreeNode, s string) {
+// 		if root.Left == nil && root.Right == nil {
+// 			s = s + strconv.Itoa(root.Val)
+// 			res = append(res, s)
+// 			return
+// 		}
+
+// 		s = s + strconv.Itoa(root.Val) + "->"
+
+// 		if root.Left != nil {
+// 			traverse(root.Left, s)
+// 		}
+
+// 		if root.Right != nil {
+// 			traverse(root.Right, s)
+// 		}
+// 	}
+
+// 	traverse(root, "")
+// 	return res
+// }
+
 func binaryTreePaths(root *TreeNode) []string {
+	stack := []*TreeNode{}
+	paths := make([]string, 0)
 	res := make([]string, 0)
+	if root != nil {
+		stack = append(stack, root)
+		paths = append(paths, "")
+	}
 
-	var traverse func(root *TreeNode, s string)
+	for len(stack) > 0 {
+		l := len(stack)
+		node := stack[l-1]
+		path := paths[l-1]
+		stack = stack[:l-1]
+		paths = paths[:l-1]
 
-	traverse = func(root *TreeNode, s string) {
-		if root.Left == nil && root.Right == nil {
-			s = s + strconv.Itoa(root.Val)
-			res = append(res, s)
-			return
+		if node.Left == nil && node.Right == nil {
+			res = append(res, path+strconv.Itoa(node.Val))
+			continue
 		}
 
-		s = s + strconv.Itoa(root.Val) + "->"
-
-		if root.Left != nil {
-			traverse(root.Left, s)
+		if node.Left != nil {
+			stack = append(stack, node.Left)
+			paths = append(paths, path+strconv.Itoa(node.Val)+"->")
 		}
 
-		if root.Right != nil {
-			traverse(root.Right, s)
+		if node.Right != nil {
+			stack = append(stack, node.Right)
+			paths = append(paths, path+strconv.Itoa(node.Val)+"->")
 		}
 	}
 
-	traverse(root, "")
 	return res
 }
