490611e6f6c3994a93d4afcb49bd87a2fd7e39d5
cjhw <1784581164@qq.com>
2023-01-17 10:52:14 +0800

    把二叉搜索树转换为累加树

---
+++ Tree/把二叉搜索树转换为累加树.go
@@ -0,0 +1,22 @@
+package Tree
+
+func convertBST(root *TreeNode) *TreeNode {
+	pre := 0
+
+	var traverse func(cur *TreeNode)
+
+	traverse = func(cur *TreeNode) {
+		if cur == nil {
+			return
+		}
+		traverse(cur.Right)
+		cur.Val += pre
+		pre = cur.Val
+		traverse(cur.Left)
+	}
+
+	traverse(root)
+
+	return root
+
+}
