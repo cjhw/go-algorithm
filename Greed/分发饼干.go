8e32ef482ab6c94548086d23cb16833482e06389
cjhw <1784581164@qq.com>
2023-01-31 17:27:21 +0800

    分发饼干

---
+++ Greed/分发饼干.go
@@ -0,0 +1,19 @@
+package Greed
+
+import "sort"
+
+func findContentChildren(g []int, s []int) int {
+
+	sort.Ints(g)
+	sort.Ints(s)
+
+	child:=0
+
+	for i:=0;i<len(s)&&child<len(g);i++ {
+		if s[i]>=g[child] {
+			child++
+		}
+	}
+
+	return child
+}
