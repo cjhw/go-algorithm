384a11176a1e1d91822e4e578a32e2b40fa07cb4
cjhw <1784581164@qq.com>
2023-02-02 09:44:45 +0800

    加油站

---
+++ Greed/加油站.go
@@ -0,0 +1,21 @@
+package Greed
+
+func canCompleteCircuit(gas []int, cost []int) int {
+	curSum := 0
+	totalSum := 0
+	start := 0
+	for i := 0; i < len(gas); i++ {
+		curSum += gas[i] - cost[i]
+		totalSum += gas[i] - cost[i]
+		if curSum < 0 {
+			curSum = 0
+			start = i + 1
+		}
+	}
+
+	if totalSum < 0 {
+		return -1
+	}
+
+	return start
+}
