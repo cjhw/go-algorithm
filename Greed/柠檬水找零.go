28f0a67baefb7352ec55c12ee90d79d0dca0f82d
cjhw <1784581164@qq.com>
2023-02-02 14:43:11 +0800

    柠檬水找零

---
+++ Greed/柠檬水找零.go
@@ -0,0 +1,27 @@
+package Greed
+
+func lemonadeChange(bills []int) bool {
+	ten, five := 0, 0
+
+	for i := 0; i < len(bills); i++ {
+		if bills[i] == 5 {
+			five++
+		} else if bills[i] == 10 {
+			if five == 0 {
+				return false
+			}
+			five--
+			ten++
+		} else {
+			if ten >= 1 && five >= 1 {
+				ten--
+				five--
+			} else if five >= 3 {
+				five -= 3
+			} else {
+				return false
+			}
+		}
+	}
+	return true
+}
