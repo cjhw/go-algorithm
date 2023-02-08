0ed0a0e6d0c8e3a37ac0a3562e3aba60e8aee24a
cjhw <1784581164@qq.com>
2023-02-08 17:53:28 +0800

    不同路径II

---
+++ Dp/不同路径II.go
@@ -0,0 +1,29 @@
+package Dp
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	m, n := len(obstacleGrid), len(obstacleGrid[0])
+
+	dp := make([][]int, m)
+
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+
+	// 初始化, 如果是障碍物, 后面的就都是0, 不用循环了
+	for i := 0; i < m && obstacleGrid[i][0] == 0; i++ {
+		dp[i][0] = 1
+	}
+	for j := 0; j < n && obstacleGrid[0][j] == 0; j++ {
+		dp[0][j] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if obstacleGrid[i][j] == 0 {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			}
+		}
+	}
+
+	return dp[m-1][n-1]
+}
